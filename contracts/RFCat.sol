e858a001aa5fc7455723fd797653f6e74b12715a
rfcat-fi <rfcat@tutanota.com>
2021-01-15 02:38:19 +0100

    Remove debug

--- contracts/RFCat.sol
+++ contracts/RFCat.sol
@@ -8,7 +8,6 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/GSN/Context.sol';
 import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
-import 'hardhat/console.sol';
 
 contract RFCat is Context, IERC20, Ownable, ReentrancyGuard {
     using SafeMath for uint256;
